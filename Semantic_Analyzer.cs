9edcb49583c40ad761ec3fbebb23771079a81e79
DHC <dariohcvg@gmail.com>
2023-07-14 18:32:17 -0400

    regions added in Semantic Analyzer code

--- Semantic_Analyzer.cs
+++ Semantic_Analyzer.cs
@@ -9,6 +9,7 @@ class Semantic_Analizer
         this.Current_Context = new Context();
     }
 
+    #region Function definition handling and type inference
     public void Define_Function(Function_Declaration_Node declaration)
     {
         SimpleType.Reset_Names();
@@ -60,7 +61,7 @@ class Semantic_Analizer
 
         var func_symbol = new Function_Symbol(func_name, return_type, Resolved_Parameters);
         func_symbol.Body = declaration.Body;
-        
+
         Current_Context.Define(func_symbol);
 
         var actual_return = typecheck(declaration.Body);
@@ -72,161 +73,6 @@ class Semantic_Analizer
 
         Current_Context.Define(func_symbol);
 
-    }
-
-    public SimpleType typecheck(AST_Treenode node)
-    {
-        if (node is NUMBER_Node) return SimpleType.NUMBER();
-        else if (node is STRING_Node) return SimpleType.STRING();
-        else if (node is BOOLEAN_Node) return SimpleType.BOOLEAN();
-        else if (node is Variable_Node)
-        {
-            var variable = (Variable_Node)node;
-            var name = variable.VarToken.Value;
-            var symbol = Current_Context.Lookup(name);
-
-            if (symbol == null)
-                throw new Exception(SEMANTIC_ERROR + Undefined_Variable_Use(name, variable.VarToken.position));
-            
-            if (symbol is not Variable_Symbol)
-                throw new Exception(SEMANTIC_ERROR + Function_Used_as_Variable(name, variable.VarToken.position));
-
-            var variable_symbol = (Variable_Symbol)symbol;
-
-            return variable_symbol.Return_Type;
-        }
-        else if (node is Function_Call_Node)
-        {
-            var call = (Function_Call_Node)node;
-            var name = call.Name_node.VarToken.Value;
-            var symbol = Current_Context.Lookup(name);
-
-            if (symbol == null)
-                throw new Exception(SEMANTIC_ERROR + Undefined_Function_Use(name, call.Name_node.VarToken.position));
-
-            if (symbol is not Function_Symbol)
-                throw new Exception(SEMANTIC_ERROR + Variable_Used_as_Function(name, call.Name_node.VarToken.position));
-
-            var func_symbol = (Function_Symbol)symbol;
-
-            if (func_symbol.Parameters.Count != call.Arguments.Count)
-                throw new Exception(SEMANTIC_ERROR + Incorrect_Number_of_Arguments(name, call.Name_node.VarToken.position, func_symbol.Parameters.Count, call.Arguments.Count));
-
-            for (int i = 0; i < call.Arguments.Count; i++)
-            {
-                var actualType = typecheck(call.Arguments[i]);
-                if (actualType != func_symbol.Parameters[i].Return_Type && func_symbol.Parameters[i].Return_Type != SimpleType.ANY())
-                {
-                    var func_name = call.Name_node.VarToken.Value;
-                    int pos = call.Name_node.VarToken.position;
-                    var expected = func_symbol.Parameters[i].Return_Type;
-            
-                    throw new Exception(SEMANTIC_ERROR + Incorrect_Type_of_Argument(func_name, pos, expected, actualType, i));
-                }
-            }
-
-            return func_symbol.Return_Type;
-        }
-        else if (node is UnOp_Node)
-        {
-            var op = (UnOp_Node)node;
-            var operand_type = typecheck(op.right);
-            if (operand_type != SimpleType.NUMBER())
-                throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Type_UnOp(operand_type, op.Operator));
-            
-            return SimpleType.NUMBER();
-        }
-        else if (node is BinOp_Node)
-        {
-            var op = (BinOp_Node)node;
-
-            var left_T = typecheck(op.left);
-            var right_T = typecheck(op.right);
-                       
-            switch(op.Operator.Value)
-            {
-            case Token.PLUS:
-            case Token.MINUS:
-            case Token.TIMES:
-            case Token.DIVISION:
-            case Token.MODULO:
-            case Token.POWER:
-                if (left_T != SimpleType.NUMBER() || right_T != left_T)
-                    throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Types_BinOp(left_T, op.Operator, right_T));
-                
-                return SimpleType.NUMBER();
-            
-            case Token.EQUAL_EQUAL:
-                // any types work here
-
-                return SimpleType.BOOLEAN();
-            
-            case Token.SMALLER:
-            case Token.GREATER:
-            case Token.GREATER_EQUAL:
-            case Token.SMALLER_EQUAL:
-                if (left_T != SimpleType.NUMBER() || right_T != left_T)
-                    throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Types_BinOp(left_T, op.Operator, right_T));
-
-                return SimpleType.BOOLEAN();
-
-            case Token.AT_OPERATOR:
-                // anything works here too
-
-                return SimpleType.STRING();
-            
-            case KeyWords.OR:
-            case KeyWords.AND:
-                if (left_T != SimpleType.BOOLEAN() || right_T != left_T)
-                    throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Types_BinOp(left_T, op.Operator, right_T));
-
-                return SimpleType.BOOLEAN();
-            
-            default:
-                throw new Exception("UNSUPPORTED OPERATOR");   
-            }
-        }
-        else if (node is If_Else_Node)
-        {
-            var if_else = (If_Else_Node)node;
-
-            var condition_T = typecheck(if_else.Condition);
-            if (condition_T != SimpleType.BOOLEAN())
-                throw new Exception(SEMANTIC_ERROR + $"{Token.BOOLEAN} expected in condition of if-else at {if_else.BeginPos}");
-            
-            var True_Clause = typecheck(if_else.True_Clause);
-            var False_Clause = typecheck(if_else.False_Clause);
-
-            if (True_Clause != False_Clause)
-                throw new Exception(SEMANTIC_ERROR + $"If-else statement at {if_else.BeginPos} has two different possible return types {True_Clause} and {False_Clause}");
-
-            return True_Clause;
-        }
-        else if (node is Let_In_Node)
-        {
-            var original_context = Current_Context;
-            
-            Current_Context = new Context(Current_Context);
-
-            var let_in = (Let_In_Node)node;
-            foreach(var declaration in let_in.Declarations)
-            {
-                var Name = declaration.Variable_Node.VarToken.Value;
-                var type = typecheck(declaration.Value);
-
-                var symbol = new Variable_Symbol(Name, type);
-                Current_Context.Define(symbol);
-            }
-
-            var answ = typecheck(let_in.Statement);
-
-            Current_Context = original_context;
-
-            return answ;
-        }
-    
-
-        else throw new Exception("TYPE Checking UNEXPECTED NODE" + node.GetType());        
     }
 
     void InferTypes(AST_Treenode node, SimpleType Expected, Equation_System Constraints)
@@ -406,8 +252,170 @@ class Semantic_Analizer
         }
         else throw new Exception("TYPE INFERENCE UNEXPECTED NODE");
     }
-    public const string SEMANTIC_ERROR = "! SEMATIC ERROR: ";
 
+    #endregion
+
+    #region Static Type Checking
+    public SimpleType typecheck(AST_Treenode node)
+    {
+        if (node is NUMBER_Node) return SimpleType.NUMBER();
+        else if (node is STRING_Node) return SimpleType.STRING();
+        else if (node is BOOLEAN_Node) return SimpleType.BOOLEAN();
+        else if (node is Variable_Node)
+        {
+            var variable = (Variable_Node)node;
+            var name = variable.VarToken.Value;
+            var symbol = Current_Context.Lookup(name);
+
+            if (symbol == null)
+                throw new Exception(SEMANTIC_ERROR + Undefined_Variable_Use(name, variable.VarToken.position));
+            
+            if (symbol is not Variable_Symbol)
+                throw new Exception(SEMANTIC_ERROR + Function_Used_as_Variable(name, variable.VarToken.position));
+
+            var variable_symbol = (Variable_Symbol)symbol;
+
+            return variable_symbol.Return_Type;
+        }
+        else if (node is Function_Call_Node)
+        {
+            var call = (Function_Call_Node)node;
+            var name = call.Name_node.VarToken.Value;
+            var symbol = Current_Context.Lookup(name);
+
+            if (symbol == null)
+                throw new Exception(SEMANTIC_ERROR + Undefined_Function_Use(name, call.Name_node.VarToken.position));
+
+            if (symbol is not Function_Symbol)
+                throw new Exception(SEMANTIC_ERROR + Variable_Used_as_Function(name, call.Name_node.VarToken.position));
+
+            var func_symbol = (Function_Symbol)symbol;
+
+            if (func_symbol.Parameters.Count != call.Arguments.Count)
+                throw new Exception(SEMANTIC_ERROR + Incorrect_Number_of_Arguments(name, call.Name_node.VarToken.position, func_symbol.Parameters.Count, call.Arguments.Count));
+
+            for (int i = 0; i < call.Arguments.Count; i++)
+            {
+                var actualType = typecheck(call.Arguments[i]);
+                if (actualType != func_symbol.Parameters[i].Return_Type && func_symbol.Parameters[i].Return_Type != SimpleType.ANY())
+                {
+                    var func_name = call.Name_node.VarToken.Value;
+                    int pos = call.Name_node.VarToken.position;
+                    var expected = func_symbol.Parameters[i].Return_Type;
+            
+                    throw new Exception(SEMANTIC_ERROR + Incorrect_Type_of_Argument(func_name, pos, expected, actualType, i));
+                }
+            }
+
+            return func_symbol.Return_Type;
+        }
+        else if (node is UnOp_Node)
+        {
+            var op = (UnOp_Node)node;
+            var operand_type = typecheck(op.right);
+            if (operand_type != SimpleType.NUMBER())
+                throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Type_UnOp(operand_type, op.Operator));
+            
+            return SimpleType.NUMBER();
+        }
+        else if (node is BinOp_Node)
+        {
+            var op = (BinOp_Node)node;
+
+            var left_T = typecheck(op.left);
+            var right_T = typecheck(op.right);
+                       
+            switch(op.Operator.Value)
+            {
+            case Token.PLUS:
+            case Token.MINUS:
+            case Token.TIMES:
+            case Token.DIVISION:
+            case Token.MODULO:
+            case Token.POWER:
+                if (left_T != SimpleType.NUMBER() || right_T != left_T)
+                    throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Types_BinOp(left_T, op.Operator, right_T));
+                
+                return SimpleType.NUMBER();
+            
+            case Token.EQUAL_EQUAL:
+                // any types work here
+
+                return SimpleType.BOOLEAN();
+            
+            case Token.SMALLER:
+            case Token.GREATER:
+            case Token.GREATER_EQUAL:
+            case Token.SMALLER_EQUAL:
+                if (left_T != SimpleType.NUMBER() || right_T != left_T)
+                    throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Types_BinOp(left_T, op.Operator, right_T));
+
+                return SimpleType.BOOLEAN();
+
+            case Token.AT_OPERATOR:
+                // anything works here too
+
+                return SimpleType.STRING();
+            
+            case KeyWords.OR:
+            case KeyWords.AND:
+                if (left_T != SimpleType.BOOLEAN() || right_T != left_T)
+                    throw new Exception(SEMANTIC_ERROR + Incorrect_Operand_Types_BinOp(left_T, op.Operator, right_T));
+
+                return SimpleType.BOOLEAN();
+            
+            default:
+                throw new Exception("UNSUPPORTED OPERATOR");   
+            }
+        }
+        else if (node is If_Else_Node)
+        {
+            var if_else = (If_Else_Node)node;
+
+            var condition_T = typecheck(if_else.Condition);
+            if (condition_T != SimpleType.BOOLEAN())
+                throw new Exception(SEMANTIC_ERROR + $"{Token.BOOLEAN} expected in condition of if-else at {if_else.BeginPos}");
+            
+            var True_Clause = typecheck(if_else.True_Clause);
+            var False_Clause = typecheck(if_else.False_Clause);
+
+            if (True_Clause != False_Clause)
+                throw new Exception(SEMANTIC_ERROR + $"If-else statement at {if_else.BeginPos} has two different possible return types {True_Clause} and {False_Clause}");
+
+            return True_Clause;
+        }
+        else if (node is Let_In_Node)
+        {
+            var original_context = Current_Context;
+            
+            Current_Context = new Context(Current_Context);
+
+            var let_in = (Let_In_Node)node;
+            foreach(var declaration in let_in.Declarations)
+            {
+                var Name = declaration.Variable_Node.VarToken.Value;
+                var type = typecheck(declaration.Value);
+
+                var symbol = new Variable_Symbol(Name, type);
+                Current_Context.Define(symbol);
+            }
+
+            var answ = typecheck(let_in.Statement);
+
+            Current_Context = original_context;
+
+            return answ;
+        }
+    
+
+        else throw new Exception("TYPE Checking UNEXPECTED NODE" + node.GetType());        
+    }
+
+    #endregion
+
+    #region Errors
+    public const string SEMANTIC_ERROR = "! SEMATIC ERROR: ";
+    
     string Type_Error(int pos, string Expected)
         => $"Expected type {Expected} at {pos}";
 
@@ -435,8 +443,12 @@ class Semantic_Analizer
     string Incorrect_Operand_Type_UnOp(SimpleType operand, Token op)
         => $"Incorrect Operand type {operand} for unary operator {op.Value} at {op.position}"; 
 
+    #endregion
+
 }
 
+
+#region Type Equations and Type Equation system
 class Equation_System
 {
     List<Equation> equations;
@@ -585,4 +597,4 @@ class Equation
 
 }
 
-// function f() => if (false) f() else 3;
+#endregion
